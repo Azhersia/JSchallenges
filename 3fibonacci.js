b8003e28fbf4f47d9761d5bcdb782ee06e75e5f4
Azhersia <alissa.ostergard@gmail.com>
2023-10-24 13:01:47 +0300

    name fixes

--- 3fibonacci.js
+++ 3fibonacci.js
@@ -4,9 +4,9 @@ let fibNmr1 = 0;
 let fibNmr2 = 1;
 let nextFibonacci;
 
-const numberofFibb = readline('How many fibonacci numbers to generate: ')
+const numberofFib = readline('How many fibonacci numbers to generate: ')
 
-for (let i = 0; i < numberofFibb; i++) {
+for (let i = 0; i < numberofFib; i++) {
 
   nextFibonacci = fibNmr1 + fibNmr2; // adds numbers and stores
   fibNmr1 = fibNmr2; // moves second value to first number
--- 9primeNmr.js
+++ 9primeNmr.js
@@ -1,3 +1,8 @@
 import readline from "read-console-input";
 
 const inputNumber = Number(readline("choose a number: "))
+
+
+if (inputNumber / 2 < 1) {
+  console.log("what up");
+}
